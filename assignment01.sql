c062fe74431ed00de33656240b113f1a1f289937
MADHUVEER <111329658+Antonyalex930@users.noreply.github.com>
2023-05-07 21:18:11 +0530

    Add files via upload

---
+++ assignment01.sql
@@ -0,0 +1,39 @@
+
+create database consulatandtherapy;
+
+CREATE TABLE Doctor (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(50),
+  specialization VARCHAR(50),
+  qualification VARCHAR(100)
+);
+
+CREATE TABLE Appointments (
+  id INT PRIMARY KEY NOT NULL,
+  doctor_id INT,
+  patient_id INT,
+  date DATE,
+  time TIME,
+  status VARCHAR(50)
+);
+
+CREATE TABLE Patient (
+  id INT PRIMARY KEY NOT NULL,
+  name VARCHAR(50),
+  email VARCHAR(50),
+  phone VARCHAR(14),
+  address VARCHAR(100)
+);
+
+CREATE TABLE Reviews (
+  id INT PRIMARY KEY NOT NULL,
+  patient_id INT,
+  doctor_id INT,
+  rating INT,
+  comment VARCHAR(200)
+);
+
+select * from Doctor;
+select * from Appointments;
+select * from Patient;
+select * from Reviews;
